a84d36e9b862e30e83746a6491e546e8c38c2486
tangchinh <chinhtv@tripsoft.io>
2023-06-08 18:29:17 +0700

    fix

--- Assets/Script/Level/LV11/Donut.cs
+++ Assets/Script/Level/LV11/Donut.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,25 +7,33 @@ public class Donut : MonoBehaviour
     public MoveLid moveLid;
     private LevelManager levelManager;
     private TickCompleteLevel tickCompleteLevel;
+    private SpriteRenderer spriteRenderer;
+    public float pouringAngleThreshold = 180f; // Góc nghiêng tối thiểu để bắt đầu đổ nước
+    private bool hasPoured = false; // Biến kiểm soát việc đã đổ nước hay chưa
     private void Start()
     {
-        gameObject.SetActive(false);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = false;
         levelManager = GameObject.FindObjectOfType<LevelManager>();
         tickCompleteLevel = GameObject.FindObjectOfType<TickCompleteLevel>();
     }
 
     private void Update()
     {
-        if (moveLid.GetNap()) {
-            if (Input.deviceOrientation == DeviceOrientation.PortraitUpsideDown)
+        if (moveLid.GetNap())
+        {
+            if (!hasPoured)
             {
-                gameObject.SetActive(true);
-                levelManager.CompleteLevel();
-                tickCompleteLevel.Tick();
-            }
-            else
-            {
-                Debug.Log("abc");
+                Vector3 acceleration = Input.acceleration;
+                float rotationZ = Mathf.Atan2(-acceleration.x, -acceleration.y) * Mathf.Rad2Deg;
+
+                // Nghiêng cốc nước theo gia tốc của thiết bị
+                transform.localRotation = Quaternion.Euler(0, 0, rotationZ);
+              
+                    spriteRenderer.enabled = true;
+                    levelManager.CompleteLevel();
+                    tickCompleteLevel.Tick();
+                
             }
         }
     }
