63883e4b41501c9a4d05c6b0bcb6dbe12221078b
tangchinh <chinhtv@tripsoft.io>
2023-06-24 15:47:56 +0700

    change audio, change imgae button

---
+++ Assets/Script/UI/Button/RestartButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RestartButton : MonoBehaviour
+{
+    private Button button;
+    public AudioClip clickButton;
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnRestartLevelButtonClick);
+    }
+    public void OnRestartLevelButtonClick()
+    {
+        AudioManager.Instance.PlaySFX(clickButton);
+        LevelManager.Instance.UpdateLevels();
+    }
+}
